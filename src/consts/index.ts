bbc7bfc64c9ec12051b63d356f4517c4aa26499b
Edwin-AM <eeam5514@outlook.com>
2024-04-15 14:31:03 -0600

    fix: Change in description texts

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -1,11 +1,2 @@
 export const ABOUT: string[] = [
-  "Soy un ingeniero de software full-stack con experiencia en el desarrollo de soluciones para el sector bancario. Actualmente, formo parte de un equipo dedicado al mantenimiento y mejora continua de una plataforma web crucial para la gestión de tarjetas de crédito y débito, utilizando Vue.js en el frontend y el lenguaje RPGLE de IBM i en el backend.",
-  "Lo que más me motiva en mi trabajo es el proceso de desarrollo de software en sí mismo. Me apasiona entender cómo funcionan las tecnologías y explorar nuevas formas de utilizarlas para crear soluciones innovadoras que puedan ser utilizadas por una amplia gama de usuarios. Mi enfoque principal radica en la solución de problemas y la investigación constante para mejorar mis habilidades y ofrecer resultados de calidad.",
-  "He contribuido a diversos proyectos en el pasado, desde sistemas académicos hasta soluciones empresariales. Destaco mi participación liderando proyectos de modernización de programas backend, e integrando tecnologías frontend como Vue.js y actualizando sistemas escritos en RPGLE."
-]
-
-export const INTERESTS: string[] = [
-  "Mi mayor pasión dentro de la ingeniería de software es la creación y desarrollo de productos tecnológicos, especialmente en el ámbito del desarrollo web full-stack. Disfruto del desafío de coordinar diferentes tecnologías para ofrecer soluciones completas y eficientes.",
-  "Fuera del ámbito laboral, me gusta mantenerme actualizado sobre temas relacionados con la informática y el desarrollo de software. Participar en comunidades en línea, seguir a creadores de contenido y suscribirme a newsletters me ayuda a estar al tanto de las últimas tendencias y avances en la industria.",
-  "Además, disfruto de actividades al aire libre como caminatas por zonas verdes, así como también de momentos de relajación como la lectura y ver películas."
-]
+  "Soy un ingeniero de software full-stack con experiencia en el desarrollo de soluciones para el sector bancario. Actualmente, formo parte de un equipo dedicado al mantenimiento y mejora continua de una plataforma web crucial para la gestión de tarjetas de crédito y débito, utilizando Vue.js en el frontend y el lenguaje RPGLE de IBM i en el backend."]
